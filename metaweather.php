ca19a9d6bad624c7dd052a7cba3ed844bc46ccf0
VivianaTe <90007736+VivianaTe@users.noreply.github.com>
2021-09-03 10:29:45 -0500

    Update metaweather.php

--- metaweather.php
+++ metaweather.php
@@ -5,32 +5,32 @@
     }
 
 
-    $curl = curl_init(); //inicia la sesión cURL
+    $curl = curl_init(); 
 
      
     $apiCall = 'https://www.metaweather.com/api/location/search/?query='.$ubicacion.'';
 
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $apiCall, //url a la que se conecta
-        CURLOPT_RETURNTRANSFER => true, //devuelve el resultado como una cadena del tipo curl_exec
-        CURLOPT_FOLLOWLOCATION => true, //sigue el encabezado que le envíe el servidor
-        CURLOPT_ENCODING => "", // permite decodificar la respuesta y puede ser"identity", "deflate", y "gzip", si está vacío recibe todos los disponibles.
-        CURLOPT_MAXREDIRS => 10, // Si usamos CURLOPT_FOLLOWLOCATION le dice el máximo de encabezados a seguir
-        CURLOPT_TIMEOUT => 30, // Tiempo máximo para ejecutar
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, // usa la versión declarada
-        CURLOPT_CUSTOMREQUEST => "GET", // el tipo de petición, puede ser PUT, POST, GET o Delete dependiendo del servicio
+        CURLOPT_URL => $apiCall, 
+        CURLOPT_RETURNTRANSFER => true, 
+        CURLOPT_FOLLOWLOCATION => true, 
+        CURLOPT_ENCODING => "", 
+        CURLOPT_MAXREDIRS => 10, 
+        CURLOPT_TIMEOUT => 30, 
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1, 
+        CURLOPT_CUSTOMREQUEST => "GET", 
 
-    )); //curl_setopt_array configura las opciones para una transferencia cURL
+    )); 
     
-    $response = curl_exec($curl);// respuesta generada
-    $err = curl_error($curl); // muestra errores en caso de existir
+    $response = curl_exec($curl);
+    $err = curl_error($curl); 
     
-    curl_close($curl); // termina la sesión 
+    curl_close($curl); 
     
     if ($err) {
-        echo "cURL Error #:" . $err; // mostramos el error
+        echo "cURL Error #:" . $err; 
     } else {
-        echo $response; // en caso de funcionar correctamente
+        echo $response; 
     }
 
 
